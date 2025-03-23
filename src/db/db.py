7d3048df383319d3254d49cef78280b57e898849
EnvyW6567 <88361948+EnvyW6567@users.noreply.github.com>
2025-03-23 14:55:36 +0900

    chore: 스타일 수정

--- src/db/db.py
+++ src/db/db.py
@@ -6,7 +6,6 @@ from src.core.config import settings
 
 engine = create_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
 
