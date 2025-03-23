ec150da85dccb138e3ade67043e800971ff6ab1a
EnvyW6567 <88361948+EnvyW6567@users.noreply.github.com>
2025-03-23 14:56:06 +0900

    feat: 서버 실행 추가

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from fastapi import FastAPI
-from dotenv import load_dotenv
 
-load_dotenv()
-app = FastAPI()
+from src.db.db import Base, engine
+from src.member.router import router as member_router
 
+Base.metadata.create_all(bind=engine)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app = FastAPI(title="grepp")
 
+app.include_router(member_router)
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
