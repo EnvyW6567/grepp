9c9439dce7a07f0a79373f52c3f07975dd6ab9e8
EnvyW6567 <88361948+EnvyW6567@users.noreply.github.com>
2025-03-24 16:13:37 +0900

    fix: default=1 -> default=0

--- src/reservation/model.py
+++ src/reservation/model.py
@@ -11,6 +11,6 @@ class Reservation(Base):
     member_id = Column(Integer, ForeignKey("member.id"), nullable=False)
     description = Column(String, nullable=False)
     date = Column(DateTime, nullable=False)
-    current_people = Column(Integer, default=1, nullable=False)
+    current_people = Column(Integer, default=0, nullable=False)
     max_people = Column(Integer, default=50000, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
