3bd7c4806fac9bc7d22dc88d049268be8134c29a
EnvyW6567 <88361948+EnvyW6567@users.noreply.github.com>
2025-03-25 21:11:42 +0900

    fate: 어플리케이션 글로벌 예외처리 등록

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from fastapi import FastAPI
 
+from src.core.exception.global_exception_middleware import GlobalExceptionMiddleware
 from src.db.db import Base, engine
+from src.exam.router import admin_router as admin_exam_router
+from src.exam.router import router as exam_router
 from src.member.router import router as member_router
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="grepp")
+app.add_middleware(GlobalExceptionMiddleware)
 
 app.include_router(member_router)
+app.include_router(exam_router)
+app.include_router(admin_exam_router)
 
 if __name__ == "__main__":
     import uvicorn
