f44f8c5615419242d1bc161b0bc8c0c4b240d917
EnvyW6567 <88361948+EnvyW6567@users.noreply.github.com>
2025-03-26 15:38:27 +0900

    feat: 서버 실행 포트 동적 반영
    - SERVER_PORT 환경변수 사용

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import os
+
+from dotenv import load_dotenv
 from fastapi import FastAPI
 
 from src.core.exception.global_exception_middleware import GlobalExceptionMiddleware
@@ -8,6 +11,8 @@ from src.exam.router import router as exam_router
 from src.member.router import router as member_router
 from src.reservation.admin_router import admin_router as admin_reservation_router
 from src.reservation.router import router as reservation_router
+
+load_dotenv()
 
 Base.metadata.create_all(bind=engine)
 
@@ -25,4 +30,4 @@ app.include_router(admin_reservation_router)
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv('SERVER_PORT', 8000)), reload=True)
